5ef28a187f1a98b602b5ac94bab7f93d0d38da8a
BokaiWang <bk.wang1989@gmail.com>
2024-04-08 21:32:43 -0400

    State Management - testing components that use context

---
+++ tests/components/Label.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import Label from "../../src/components/Label";
+import { LanguageProvider } from "../../src/providers/language/LanguageProvider";
+import { Language } from "../../src/providers/language/type";
+
+describe("Label", () => {
+  const renderComponent = (labelId: string, language: Language) => {
+    render(
+      <LanguageProvider language={language}>
+        <Label labelId={labelId} />
+      </LanguageProvider>
+    );
+  };
+  describe("The given language is English", () => {
+    it.each([
+      { labelId: "welcome", text: "Welcome" },
+      { labelId: "new_product", text: "New Product" },
+      { labelId: "edit_product", text: "Edit Product" },
+    ])("should render $text when keyword is $labelId", ({ labelId, text }) => {
+      renderComponent(labelId, "en");
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+  });
+
+  describe("The given language is Spanish", () => {
+    it.each([
+      { labelId: "welcome", text: "Bienvenidos" },
+      { labelId: "new_product", text: "Nuevo Producto" },
+      { labelId: "edit_product", text: "Editar Producto" },
+    ])("should render $text when keyword is $labelId", ({ labelId, text }) => {
+      renderComponent(labelId, "es");
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+  });
+
+  it("should throw an error when given an invalid labelId", () => {
+    expect(() => renderComponent("!", "en")).toThrowError();
+  });
+});
