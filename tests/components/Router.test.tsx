6d62c15ead24a27c13a125fcb19dc6a443f5b09f
BokaiWang <bk.wang1989@gmail.com>
2024-04-14 18:23:16 -0400

    Testing Routing - testing routes with parameters

--- tests/components/Router.test.tsx
+++ tests/components/Router.test.tsx
@@ -1,7 +1,9 @@
 import { screen } from "@testing-library/react";
+import { db } from "../mocks/db";
 import { navigateTo } from "../utilities";
 
 describe("Router", () => {
+  beforeAll(() => {});
   it("should render home page for /", () => {
     navigateTo("/");
 
@@ -14,5 +16,16 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render a product details page for /products/:id when the product exists", async () => {
+    const product = db.product.create();
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
