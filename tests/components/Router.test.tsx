a3ce4b3faaea744c1cadc50f6b159925be716c1a
BokaiWang <bk.wang1989@gmail.com>
2024-04-14 18:05:28 -0400

    Testing Routing - extracting navigateTo

--- tests/components/Router.test.tsx
+++ tests/components/Router.test.tsx
@@ -1,20 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import routes from "../../src/routes";
+import { screen } from "@testing-library/react";
+import { navigateTo } from "../utilities";
 
 describe("Router", () => {
   it("should render home page for /", () => {
-    const router = createMemoryRouter(routes, { initialEntries: ["/"] });
-    render(<RouterProvider router={router} />);
+    navigateTo("/");
 
     expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
   });
 
   it("should render products page for /products", () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/products"],
-    });
-    render(<RouterProvider router={router} />);
+    navigateTo("/products");
 
     expect(
       screen.getByRole("heading", { name: /products/i })
